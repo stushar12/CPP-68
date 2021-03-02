0c715d10199909a2c51eebb19477caa5af0be94b
Tushar Saxena <stushar345@gmail.com>
2021-03-02 13:30:21 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,16 +1,74 @@
-void abc(struct Node *root, int k,vector<int> &v)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
 {
-    if(root ==NULL)
-    return;
-    if(k==0)
-    v.push_back(root->data);
-    else
-    abc(root->left,k-1,v);
-    abc(root->right,k-1,v);
+	int data; 
+	Node* left;
+	Node* right;
+};
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
 }
-vector<int> Kdistance(struct Node *root, int k)
+
+Node* Insert(Node* root,int data) 
 {
- vector<int> v;
- abc(root,k,v);
- return v;
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert in right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+void distance_k_from_root(Node* root,int k)
+{
+	if(root==NULL)
+	return; 
+	else
+	{
+	if(k==0)
+	{
+		cout<<root->data<<" ";
+		return;
+	}
+	else
+	{
+		distance_k_from_root(root->left,k-1);
+		distance_k_from_root(root->right,k-1);
+	}
+
+	}
+}
+
+
+
+int main() 
+{
+	Node* root = NULL;  // Creating an empty tree
+
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,25);
+	root = Insert(root,8);
+	root = Insert(root,12);
+
+	int distance_from_root=2;
+
+	distance_k_from_root(root,distance_from_root);
 }
